40a234b5bea6ed5cf564c546f06a8c5a9657c944
King <kbmajor80@gmail.com>
2020-08-24 21:20:52 -0400

    Adding contact form javascript.

---
+++ assets/index.js
@@ -0,0 +1,49 @@
+//NOTES:
+//$(window).on("load", function(){
+// Handler when all assets (including images) are loaded
+//});
+var callback = function(){
+  // Handler when the DOM is fully loaded
+    const contactLink = document.getElementById('menu-item-003')
+    const contactForm = document.querySelector('form')
+    const contactWidgets = document.querySelector('.widget-container-container')
+    const navbarContact = document.getElementById('nav-menu-item-3')
+    const contactFormSubmit = document.getElementById('formSubmit')
+
+    contactLink.addEventListener('click', e => {
+        e.preventDefault()
+        if (e) {
+                contactForm.classList.remove('hidden-form'); 
+                contactWidgets.classList.add('hidden-widgets')  
+            }
+    })
+    
+    navbarContact.addEventListener('click', e => {
+        e.preventDefault()
+        if (e) {
+                contactForm.classList.remove('hidden-form'); 
+                contactWidgets.classList.add('hidden-widgets')  
+            }
+    })
+    
+    contactFormSubmit.addEventListener('click', e => {
+        e.preventDefault()
+        if (e) {
+                contactForm.classList.add('hidden-form'); 
+                contactWidgets.classList.remove('hidden-widgets')  
+            }
+    })
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  callback();
+} else {
+  document.addEventListener("DOMContentLoaded", callback);
+}
+
+
+
+
